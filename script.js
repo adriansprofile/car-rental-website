3811a81d508b440f6a8b360991aa41e414c54431
adriansprofile <adrianpetrusca2@gmail.com>
2025-05-16 14:13:20 +0300

    Scroll Reveal

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ document.querySelector(".close-btn").addEventListener("click", toggleMenu);
 document
 	.querySelectorAll(".menu a")
 	.forEach((link) => link.addEventListener("click", toggleMenu));
+
+// Scroll Reveal
+const sr = ScrollReveal({
+	origin: "bottom",
+	distance: "40px",
+	duration: 800,
+	delay: 200,
+	easing: "ease-in-out"
+});
+
+sr.reveal(".hero-headlines", {origin: "left" });
+sr.reveal(".hero-page img", {origin: "right" });
+sr.reveal(".about");
+sr.reveal(".about h1", {delay: "400" });
+sr.reveal(".about p", {delay: "600" });
+sr.reveal(".about-info", {delay: "800" });
+sr.reveal(".collection h1");
+sr.reveal(".collection-container", {delay: "500" });
+sr.reveal(".review h1");
+sr.reveal(".review-container", {delay: "500" });
+sr.reveal(".callout");
+sr.reveal(".contact");
